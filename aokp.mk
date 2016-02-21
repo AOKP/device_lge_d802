7c3b3b913b6e196ab10fe318810859005becad1d
GalaticStryder <icarohoff@gmail.com>
2016-02-21 11:32:15 -0300

    d802: Inline for AOKP official builds
    Signed-off-by: GalaticStryder <icarohoff@gmail.com>

--- aokp.mk
+++ aokp.mk
@@ -1,15 +1,15 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/aokp/configs/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/d802/d802.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d802
-PRODUCT_NAME := cm_d802
+PRODUCT_NAME := aokp_d802
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D802
 PRODUCT_MANUFACTURER := lge
